933705a69adc96d27716a11aeffee84efaf19900
alihashemabadi <ali_hashem_abadi@yahoo.com>
2019-04-20 00:04:58 -0400

    Checking if my git works!!!!!!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Hello everybody");
         int x;
          x = 5;
          System.out.println(x + 1);
+
     }
 }
